30ce2aa00e8d3387b7eb9f4d088c25e8db70c642
kkkk <ggn.f763@gmail.com>
2025-07-22 00:04:20 +0300

    feat: верстка компонента навигации

--- app/page.tsx
+++ app/page.tsx
@@ -4,28 +4,46 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <header>
-        <div className="header">
-          <nav>
+      <header className={styles.header}>
+        <div className={styles.header__wrapper}>
+          <nav className={styles.navigation}>
             <ul>
               <li>
-                <a href="">section 1</a>
+                <a href="#">Наклейки</a>
               </li>
               <li>
-                <a href="">section 2</a>
+                <a href="#">Преимущества</a>
+              </li>
+              <li>
+                <a href="#">Контакты</a>
               </li>
             </ul>
           </nav>
-          <Image className={styles.logo} src="/Logo.png" alt="Next.js logo" width={418} height={388} priority />
+          <Image className={styles.logo} src="/Logo.png" alt="Логотип Макулатура" width={100} height={95} priority />
         </div>
       </header>
       <main className={styles.main}>
-        <section>
-          <h1>Effortless </h1>
-          <h2>Effortless </h2>
-          <h3>Effortless </h3>
-          <h4>Effortless </h4>
-          <h5>Effortless </h5>
+        <section className={`${styles.main__section} ${styles.main__section_intro}`}>
+          <h1>Наклейки на банковскую карту</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam unde itaque harum nemo officia dolorem nihil, eum corporis, placeat cum
+            perspiciatis, blanditiis fugiat. Aut facilis aliquid inventore libero fugiat laborum ducimus dicta pariatur, sed ut debitis sapiente
+            dolorum odio harum, quidem exercitationem rem ex modi culpa? Facere accusantium iste cum adipisci, quo quis dolore animi sit ipsa aut
+            repudiandae recusandae aperiam corporis quam eligendi incidunt sequi doloribus delectus vel voluptate harum cumque vero? Excepturi nihil
+            voluptatum adipisci odio expedita id natus dolor. Aliquam recusandae earum repudiandae. Voluptatibus vel, iusto molestiae quod eius illo
+            ea quaerat maxime quo vero qui excepturi minus veniam minima perferendis aspernatur aut! Adipisci a enim quibusdam exercitationem qui
+            error neque velit temporibus, deleniti maxime. Accusamus illum sit cumque asperiores mollitia laboriosam. Inventore a dignissimos quam
+            suscipit eveniet quis ut ducimus enim libero, facere veritatis alias tenetur sed illo totam labore recusandae sapiente necessitatibus iste
+            eligendi quaerat error repudiandae ad. Illum unde iure aspernatur repellat laboriosam, sed, repellendus fuga recusandae voluptates nemo ut
+            doloribus maiores ab perspiciatis minima error. Sed quos, dicta consequatur molestias harum voluptatem magni, amet soluta ab delectus
+            minima aliquam aperiam fugit nesciunt reprehenderit voluptatum provident repellendus. Hic unde dolores iusto omnis, veniam officiis sit
+            nemo doloribus dolor tempore saepe velit rem blanditiis facilis molestiae autem perferendis natus perspiciatis. Eligendi ipsum unde
+            commodi blanditiis quasi quaerat pariatur minus soluta, facere autem dolorem atque fugit cumque, inventore eveniet dolores sed eos
+            similique minima asperiores iure. Harum beatae corporis iusto provident aut voluptatum exercitationem praesentium quidem doloremque iste
+            ipsam unde neque, similique corrupti, explicabo modi fuga sint cupiditate recusandae quod optio sit accusantium voluptatem. Debitis
+            aspernatur officia voluptatem molestiae accusantium! Nemo eius autem a maxime minus commodi fugiat voluptatem, facere doloremque
+            cupiditate odio praesentium quaerat in deleniti dicta ullam qui, exercitationem maiores veniam pariatur accusantium nihil.
+          </p>
         </section>
       </main>
       <footer className={styles.footer}>footer</footer>
