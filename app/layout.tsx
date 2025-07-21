3a488870b62849c7411a3ab744431b874931b72f
kkkk <ggn.f763@gmail.com>
2025-07-22 01:01:50 +0300

    chore: замена шрифта Montserrat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Montserrat_Alternates } from "next/font/google";
 import "./globals.css";
 
-const montserrat = Montserrat({
+const montserrat = Montserrat_Alternates({
   subsets: ["latin", "cyrillic"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   style: ["normal", "italic"],
