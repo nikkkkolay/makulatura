7fbae6977fcb6e44e78ca398c3f3687b382f939c
kkkk <ggn.f763@gmail.com>
2025-07-23 22:21:21 +0300

    feat: добавил стили для disabled элементов + сообщение об отправке

--- components/Feedback/Feedbak.tsx
+++ components/Feedback/Feedbak.tsx
@@ -18,10 +18,12 @@ export const Feedback: FC<Props> = ({ className }) => {
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure aliquam odio facere saepe veniam earum consequuntur incidunt blanditiis
         deleniti eaque!
       </p>
-      <TextField className={styles.textField} id="email" type="email" label="Почта *" required placeholder="Ваша почта" />
-      <TextField className={styles.textField} id="message" label="Сообщение *" placeholder="Напишите что-нибудь..." multiline required />
+      <TextField disabled className={styles.textField} id="email" type="email" label="Почта *" required placeholder="Ваша почта" />
+      <TextField disabled className={styles.textField} id="message" label="Сообщение *" placeholder="Напишите что-нибудь..." multiline required />
+      <p className={styles.submitMessage}>Спасибо за ваше сообщение. Сообщение отправлено!</p>
+
       <div className={styles.formFooter}>
-        <Button type="submit" size="sm">
+        <Button type="submit" size="sm" disabled>
           Отправить
         </Button>
         <p>* обязательно</p>
