b2c4dce5a09af3f485e8b286387f8b4e75a647a7
Гагара Николай Федорович <gagaranf@mauniver.ru>
2025-08-11 16:05:22 +0300

    style: линтинг проекта

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import { ProductCard, Feedback, Intro, Hero, Stickers, Shop } from "@/components";
+import { Feedback, Intro, Hero, Stickers, Shop } from "@/components";
 import styles from "./page.module.css";
 
 export default function Home() {
