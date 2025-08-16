105e3fcc3e17a6ab332ff16c550a3de5d3e754a1
kkkk <ggn.f763@gmail.com>
2025-08-16 18:18:51 +0300

    fix: поменял порт для прода

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const apiUrl = process.env.API || "http://localhost:1337";
+
 const nextConfig: NextConfig = {
   images: {
-    domains: [new URL(process.env.API as string).hostname],
+    domains: [new URL(apiUrl).hostname],
   },
 };
 
 export default nextConfig;
+
+console.log("API =", process.env.API);
